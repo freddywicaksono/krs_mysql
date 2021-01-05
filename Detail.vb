34f56a4939bcd490557af2c1facef5c4dc35b396
Freddy Wicaksono <59552422+freddywicaksono@users.noreply.github.com>
2021-01-05 10:13:59 +0700

    Add files via upload

---
+++ Detail.vb
@@ -0,0 +1,40 @@
+﻿Public Class Detail
+    Private Sub ClearEntry()
+        txtkodemk.Text = ""
+        txtNamaMK.Text = ""
+        txtSKS.Text = ""
+    End Sub
+    Private Sub Reload()
+        oKrs.getAllDataDetail(DataGridView1, txtnomor_bukti.Text)
+    End Sub
+
+    Private Sub SimpanKRSDetail()
+        oKrs.nomor_bukti = txtnomor_bukti.Text
+        oKrs.kodemk = txtkodemk.Text
+        oKrs.sks = txtSKS.Text
+        krsdetail_baru = True
+        oKrs.SimpanDetail()
+        ClearEntry()
+        Reload()
+    End Sub
+
+    Private Sub Detail_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        txtnomor_bukti.Text = nobukti
+        Reload()
+    End Sub
+
+    Private Sub txtkodemk_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles txtkodemk.KeyDown
+        If (e.KeyCode = Keys.Enter And txtkodemk.Text <> "") Then
+            oMatakuliah.Carimatakuliah(txtkodemk.Text)
+            txtNamaMK.Text = oMatakuliah.namamk
+            txtSKS.Text = oMatakuliah.sks
+        End If
+    End Sub
+
+
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+        SimpanKRSDetail()
+        txtkodemk.Focus()
+
+    End Sub
+End Class
---
+++ MyMod.vb
@@ -0,0 +1,76 @@
+﻿Imports MySql.Data.MySqlClient
+Module MyMod
+    Public myCommand As New MySqlCommand
+    Public myAdapter As New MySqlDataAdapter
+    Public myData As New DataTable
+    Public DR As MySql.Data.MySqlClient.MySqlDataReader
+    Public SQL As String
+    Public conn As New MySql.Data.MySqlClient.MySqlConnection
+
+    Public dosen_baru As Boolean
+    Public oDosen As New Dosen
+
+    Public fakultas_baru As Boolean
+    Public oFakultas As New Fakultas
+
+    Public krs_baru As Boolean
+    Public krsdetail_baru As Boolean
+    Public oKrs As New Krs
+
+    Public mahasiswa_baru As Boolean
+    Public oMahasiswa As New Mahasiswa
+
+    Public matakuliah_baru As Boolean
+    Public oMatakuliah As New Matakuliah
+
+    Public prodi_baru As Boolean
+    Public oProdi As New Prodi
+
+    Public nobukti As Double
+    Public R As Random = New Random
+
+    Public Sub DBConnect()
+        conn.ConnectionString = "server=localhost;" & _
+        "user id=root;" & _
+        "password=;" & _
+        "database=dbmaster"
+        Try
+            conn.Open()
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            If (conn.State = ConnectionState.Open) Then
+            Else
+                MessageBox.Show("Sorry not connected.")
+            End If
+        End Try
+    End Sub
+
+    Public Sub DBDisconnect()
+
+        If (conn.State = ConnectionState.Open) Then
+            conn.Close()
+            conn.Dispose()
+        End If
+    End Sub
+
+    Public Function getMD5Hash(ByVal strToHash As String) As String
+
+        Dim md5Obj As New System.Security.Cryptography.MD5CryptoServiceProvider()
+        Dim bytesToHash() As Byte = System.Text.Encoding.ASCII.GetBytes(strToHash)
+        bytesToHash = md5Obj.ComputeHash(bytesToHash)
+        Dim strResult As String = ""
+        Dim b As Byte
+        For Each b In bytesToHash
+            strResult += b.ToString("x2")
+        Next
+        Return strResult
+    End Function
+    
+
+    Public Function getNomorBukti()
+        Dim res As Double
+        res = R.Next(1000000, 9999999)
+        Return res
+    End Function
+End Module
