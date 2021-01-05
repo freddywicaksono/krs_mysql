5d3249606dc0e0f3cbe70664d2aa098b3209439b
Freddy Wicaksono <59552422+freddywicaksono@users.noreply.github.com>
2021-01-05 10:25:33 +0700

    Add files via upload

--- Detail.vb
+++ Detail.vb
@@ -37,4 +37,8 @@
         txtkodemk.Focus()
 
     End Sub
+
+    Private Sub btnClose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
 End Class
